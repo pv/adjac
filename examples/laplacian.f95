48725faa9c9b8713ae642b5ccc99ed52389bffb3
Pauli Virtanen <pav@iki.fi>
2014-10-14 15:42:46 +0300

    Swap order of arguments in COO, CSR, and indptr <-> indices

--- examples/laplacian.f95
+++ examples/laplacian.f95
@@ -9,8 +9,8 @@ program laplacian
 
   double precision, dimension(n) :: x_value
   double precision, dimension(n) :: y_value
-  integer, allocatable, dimension(:) :: jac_i, jac_j, jac_ind, jac_indptr
   double precision, allocatable, dimension(:) :: jac_val, jac_val2
+  integer, allocatable, dimension(:) :: jac_i, jac_j, jac_indices, jac_indptr
   double precision, dimension(n,n) :: jac_dense
   
   type(adjac_double), dimension(n) :: x
@@ -44,17 +44,17 @@ program laplacian
   ! Obtain jacobian in sparse coordinate (i, j, value) format
   write(*,*) '-- COO laplacian'
   nnz = adjac_get_nnz(y)
-  allocate(jac_i(nnz), jac_j(nnz), jac_val(nnz))
-  call adjac_get_coo_jacobian(y, jac_i, jac_j, jac_val)
+  allocate(jac_val(nnz), jac_i(nnz), jac_j(nnz))
+  call adjac_get_coo_jacobian(y, jac_val, jac_i, jac_j)
   do j = 1, size(jac_i,1)
      write(*,*) jac_i(j), jac_j(j), real(jac_val(j))
   end do
 
   ! Obtain jacobian in sparse CSR format
   write(*,*) '-- CSR laplacian'
-  allocate(jac_ind(n+1), jac_indptr(nnz), jac_val2(nnz))
-  call adjac_get_csr_jacobian(y, jac_ind, jac_indptr, jac_val2)
-  write(*,*) jac_ind
-  write(*,*) jac_indptr
+  allocate(jac_val2(nnz), jac_indices(nnz), jac_indptr(n+1))
+  call adjac_get_csr_jacobian(y, jac_val2, jac_indices, jac_indptr)
   write(*,*) jac_val2
+  write(*,*) jac_indices
+  write(*,*) jac_indptr
 end program laplacian
